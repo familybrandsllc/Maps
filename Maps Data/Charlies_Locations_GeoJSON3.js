4954986b41616f10bb5c6bdd99effd666cb336f4
familybrandsllc <86477587+familybrandsllc@users.noreply.github.com>
2021-07-02 17:45:37 -0400

    Create Charlies_Locations_GeoJSON3.js

---
+++ Maps Data/Charlies_Locations_GeoJSON3.js
@@ -0,0 +1,16 @@
+let stores = [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.956548,35.904038 ]
+    },
+    "properties": {
+    "Name":"Food City",
+    "Street":"6710 Malone Creek Drive",
+    "City":"Knoxville",
+    "State":"TN",
+    "Zip":37931
+    }
+  }
+]
