a90b992a3af8b65f0a0fb66f5cd7352505de5b3d
familybrandsllc <86477587+familybrandsllc@users.noreply.github.com>
2021-09-27 09:13:13 -0400

    Create LocalMarketLocations_2.js

---
+++ LocalMarketLocations_2.js
@@ -0,0 +1,392 @@
+let stores = [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.980497,35.750603 ]
+    },
+    "properties": {
+    "Name":"The Local Market 102",
+    "Street":"608 W Lamar Alexander Pky",
+    "City":"Maryville",
+    "State":"TN",
+    "Zip":37804,
+    "Phone":"(865) 380-0909"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.773712,36.004568 ]
+    },
+    "properties": {
+    "Name":"The Local Market 104",
+    "Street":"7401 Strawberry Plains Pk",
+    "City":"Knoxville",
+    "State":"TN",
+    "Zip":37914,
+    "Phone":"(865) 933-6311"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.918064,35.965139 ]
+    },
+    "properties": {
+    "Name":"The Market",
+    "Street":"504 S Gay Street",
+    "City":"Knoxville",
+    "State":"TN",
+    "Zip":37902,
+    "Phone":"(865) 541-5150"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.983768,35.769734 ]
+    },
+    "properties": {
+    "Name":"The Local Market 108",
+    "Street":"511 Louisville Rd",
+    "City":"Alcoa",
+    "State":"TN",
+    "Zip":37701,
+    "Phone":"(865) 984-9360"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.084822,35.986112 ]
+    },
+    "properties": {
+    "Name":"Aztex 112",
+    "Street":"7322 Oak Ridge Hwy",
+    "City":"Knoxville",
+    "State":"TN",
+    "Zip":37931,
+    "Phone":"(865) 690-9806"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.969898,36.011935 ]
+    },
+    "properties": {
+    "Name":"The Local Market 121",
+    "Street":"121 Cedar Lane",
+    "City":"Knoxville",
+    "State":"TN",
+    "Zip":37912,
+    "Phone":"(865) 687-3211"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.078236,35.914163 ]
+    },
+    "properties": {
+    "Name":"The Local Market 122",
+    "Street":"8870 Kingston Pike",
+    "City":"Knoxville",
+    "State":"TN",
+    "Zip":37923,
+    "Phone":"(865) 694-0942"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.637837,36.020255 ]
+    },
+    "properties": {
+    "Name":"The Local Market 126",
+    "Street":"536 Asheville Hwy",
+    "City":"Strawberry Plains",
+    "State":"TN",
+    "Zip":37871,
+    "Phone":"(865) 932-6664"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.362152,35.51487 ]
+    },
+    "properties": {
+    "Name":"Aztex 128",
+    "Street":"4500 Hwy 411, Unit #5",
+    "City":"Madisonville",
+    "State":"TN",
+    "Zip":37354,
+    "Phone":"(423) 420-0008"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.342015,35.530256 ]
+    },
+    "properties": {
+    "Name":"The Local Market129",
+    "Street":"3987 Hwy 411",
+    "City":"Madisonville",
+    "State":"TN",
+    "Zip":37354,
+    "Phone":"(423) 442-4064"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.494243,35.427291 ]
+    },
+    "properties": {
+    "Name":"Aztex 130",
+    "Street":"317 W Athens St",
+    "City":"Englewood",
+    "State":"TN",
+    "Zip":37329,
+    "Phone":"(423) 887-6923"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.534548,35.347928 ]
+    },
+    "properties": {
+    "Name":"The Local Market 131",
+    "Street":"1030 Lillard Memorial Hwy",
+    "City":"Etowah",
+    "State":"TN",
+    "Zip":37331,
+    "Phone":"(423) 263-3277"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.335547,35.738344 ]
+    },
+    "properties": {
+    "Name":"The Local Market 132",
+    "Street":"900 Grove St",
+    "City":"Loundon",
+    "State":"TN",
+    "Zip":37774,
+    "Phone":"(865) 458-4025"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.585116,35.446391 ]
+    },
+    "properties": {
+    "Name":"Aztex 136",
+    "Street":"807 Eastanallee Ave",
+    "City":"Athens",
+    "State":"TN",
+    "Zip":37303,
+    "Phone":"(423) 745-2593"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.63964,35.43907 ]
+    },
+    "properties": {
+    "Name":"Aztex 137",
+    "Street":"2506 Hwy 11S",
+    "City":"Athens",
+    "State":"TN",
+    "Zip":37303,
+    "Phone":"(423) 744-8065"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.584854,35.431563 ]
+    },
+    "properties": {
+    "Name":"Aztex 138",
+    "Street":"997 S White St",
+    "City":"Athens",
+    "State":"TN",
+    "Zip":37303,
+    "Phone":"(423) 745-9068"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.743937,35.296431 ]
+    },
+    "properties": {
+    "Name":"Aztex 139",
+    "Street":"789 Highway 163",
+    "City":"Calhoun",
+    "State":"TN",
+    "Zip":37309,
+    "Phone":"(423) 336-2194"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -85.008746,35.493597 ]
+    },
+    "properties": {
+    "Name":"The Local Market 140",
+    "Street":"106 3rd Avenue",
+    "City":"Dayton",
+    "State":"TN",
+    "Zip":37321,
+    "Phone":"(423) 775-3843"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -85.022986,35.483576 ]
+    },
+    "properties": {
+    "Name":"The Local Market 141",
+    "Street":"156 Iowa Ave",
+    "City":"Dayton",
+    "State":"TN",
+    "Zip":37321,
+    "Phone":"(423) 775-4060"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.368737,35.182259 ]
+    },
+    "properties": {
+    "Name":"The Local Market 201",
+    "Street":"329 Highland Rd",
+    "City":"Franklin",
+    "State":"NC",
+    "Zip":28734,
+    "Phone":"(828) 369-5252"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.726435,35.030485 ]
+    },
+    "properties": {
+    "Name":"The Local Market 202",
+    "Street":"Hwy 64 Bypass",
+    "City":"Hayesville",
+    "State":"NC",
+    "Zip":28904,
+    "Phone":"(828) 389-6828"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.801794,35.318455 ]
+    },
+    "properties": {
+    "Name":"The Local Market 203",
+    "Street":"129 S Tallulah Rd",
+    "City":"Robbinsville",
+    "State":"NC",
+    "Zip":28711,
+    "Phone":"(828) 479-3113"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.98875,35.48871 ]
+    },
+    "properties": {
+    "Name":"The Local Market 204",
+    "Street":"5171 J-Creek Rd",
+    "City":"Waynesville",
+    "State":"NC",
+    "Zip":28786,
+    "Phone":"(828) 926-3762"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.834362,35.201403 ]
+    },
+    "properties": {
+    "Name":"The Local Market 207",
+    "Street":"427 Main St",
+    "City":"Andrews",
+    "State":"NC",
+    "Zip":28901,
+    "Phone":"(828) 321-6055"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.449734,35.426694 ]
+    },
+    "properties": {
+    "Name":"The Local Market 210",
+    "Street":"110 Hwy 19 S",
+    "City":"Bryson City",
+    "State":"NC",
+    "Zip":28713,
+    "Phone":"(828) 488-6455"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.042947,35.077323 ]
+    },
+    "properties": {
+    "Name":"The Local Market 217",
+    "Street":"870 West US 64",
+    "City":"Murphy",
+    "State":"NC",
+    "Zip":28906,
+    "Phone":"(828) 837-5476"
+    }
+  }
+]
